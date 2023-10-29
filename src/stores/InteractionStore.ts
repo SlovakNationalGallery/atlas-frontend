0690daba9f29951d47c742f16bd21534dc3ec898
Mayo Horkovic <mayo.horkovic@gmail.com>
2023-10-29 19:56:02 +0100

    InteractionStore

--- src/stores/InteractionStore.ts
+++ src/stores/InteractionStore.ts
@@ -1,110 +1,171 @@
-import { useStorage } from '@vueuse/core'
 import { defineStore } from 'pinia'
 
-export const useInteractionStore = defineStore('InteractionStore', {
-  state: () => ({
-    interactions: useStorage('interactions', []),
-    peekCodePanel: useStorage('peekCodePanel', true),
-    cursor: useStorage('cursor', -1),
-  }),
-  getters: {
-    active() {
-      if (this.cursor > -1) {
-        return this.interactions[this.cursor]
+import type { ILink } from '@/models/_Interfaces'
+
+import Interaction from '@/models/Interaction'
+
+export const useInteractionStore = defineStore(
+  'InteractionStore',
+  () => {
+    const interactions = ref<Interaction[]>([])
+    const peekCodePanel = ref(true)
+    const cursor = ref(-1)
+
+    const active = computed(() => {
+      if (cursor.value > -1) {
+        return interactions.value[cursor.value]
       }
-    },
-    activeStory() {
-      if (this.active && this.active.type === 'story') {
+    })
+
+    const activeStory = computed(() => {
+      if (active.value !== undefined && active.value.type === 'story') {
         const storiesStore = useStoryStore()
-        return storiesStore.get(this.active.id)
+        return storiesStore.get(active.value!.id)
       }
-    },
-    lastStory() {
-      return this.stories[this.stories.length - 1]
-    },
-    stories() {
+    })
+
+    const lastStory = computed(() => {
+      return stories.value[stories.value.length - 1]
+    })
+
+    const stories = computed(() => {
       const storiesStore = useStoryStore()
-      return this.interactions
+
+      return interactions.value
         .filter((interaction) => interaction.type === 'story')
         .map((interaction) => storiesStore.get(interaction.id))
-    },
-    viewedItemIds() {
+    })
+
+    const viewedItemIds = computed(() => {
       return new Set(
-        this.interactions
+        interactions.value
           .filter((interaction) => interaction.type === 'itemViewed')
           .map((interaction) => interaction.id)
           .slice()
           .reverse()
       )
-    },
-    viewedItemsCount() {
-      return this.viewedItemIds.size
-    },
-  },
-  actions: {
-    isItemViewed(id) {
-      return this.viewedItemIds.has(id)
-    },
-    addStory(id) {
-      const length = this.interactions.push({
-        type: 'story',
-        id,
-      })
-      this.cursor = length - 1
-    },
-    addItemViewed(id) {
-      this.interactions.push({
-        type: 'itemViewed',
-        id,
-      })
-    },
-    addSectionViewed(id) {
-      this.interactions.push({
-        type: 'sectionViewed',
-        id,
-      })
-    },
-    addPlaceViewed(id) {
-      this.interactions.push({
-        type: 'placeViewed',
-        id,
-      })
-    },
-    selectLink(interaction, link) {
-      const index = this.interactions.indexOf(interaction)
-      this.interactions[index].linkId = link.id
-      this.cursor = -1
-    },
-    lastStoryIndex(from) {
-      let index = typeof from !== 'undefined' ? from : this.cursor
-      while (this.interactions[index].type !== 'story' && index > 0) {
+    })
+
+    const viewedItemsCount = computed(() => {
+      return viewedItemIds.value.size
+    })
+
+    function isItemViewed(id: string) {
+      return viewedItemIds.value.has(id)
+    }
+
+    function addStory(id: string) {
+      const length = interactions.value.push(
+        new Interaction({
+          type: 'story',
+          id,
+        })
+      )
+
+      cursor.value = length - 1
+    }
+
+    function addItemViewed(id: string) {
+      interactions.value.push(
+        new Interaction({
+          type: 'itemViewed',
+          id,
+        })
+      )
+    }
+
+    function addSectionViewed(id: string) {
+      interactions.value.push(
+        new Interaction({
+          type: 'sectionViewed',
+          id,
+        })
+      )
+    }
+
+    function addPlaceViewed(id: string) {
+      interactions.value.push(
+        new Interaction({
+          type: 'placeViewed',
+          id,
+        })
+      )
+    }
+
+    function selectLink(interaction: Interaction, link: ILink) {
+      const index = interactions.value.indexOf(interaction)
+      interactions.value[index].linkId = link.id
+      cursor.value = -1
+    }
+
+    function lastStoryIndex(from: number) {
+      let index = typeof from !== 'undefined' ? from : cursor.value
+      while (interactions.value[index].type !== 'story' && index > 0) {
         index--
       }
       return index
-    },
-    setPreviousActive(interaction) {
-      const index = this.interactions.indexOf(interaction)
-      this.cursor = this.lastStoryIndex(index - 1)
-      this.active.linkId = undefined
-      return this.active
-    },
-    remove(interaction) {
-      const toDelete = this.interactions.indexOf(interaction)
-      this.interactions.splice(toDelete, 1)
-      if (toDelete <= this.cursor) {
-        this.cursor = this.lastStoryIndex(this.cursor - 1)
+    }
+
+    function setPreviousActive(interaction: Interaction) {
+      const index = interactions.value.indexOf(interaction)
+      cursor.value = lastStoryIndex(index - 1)
+
+      if (active.value) {
+        active.value.linkId = null
       }
-    },
-    isVisited(id) {
-      return this.stories.some((interaction) => interaction.id === id)
-    },
-    hasVisitedAllLinks(id) {
+
+      return active.value
+    }
+
+    function remove(interaction: Interaction) {
+      const toDelete = interactions.value.indexOf(interaction)
+      interactions.value.splice(toDelete, 1)
+      if (toDelete <= cursor.value) {
+        cursor.value = lastStoryIndex(cursor.value - 1)
+      }
+    }
+
+    function isVisited(id: string) {
+      return stories.value.some((interaction) => interaction?.id === id)
+    }
+
+    function hasVisitedAllLinks(id: string) {
       const storyStore = useStoryStore()
       const story = storyStore.get(id)
-      return story?.links.every((link) => this.isVisited(link.story_id))
-    },
-    clear() {
-      this.interactions = []
-    },
+      return story?.links.every((link) => isVisited(link.story_id))
+    }
+
+    function clear() {
+      interactions.value = []
+    }
+
+    return {
+      interactions,
+      peekCodePanel,
+      cursor,
+
+      active,
+      activeStory,
+      lastStory,
+      stories,
+      viewedItemIds,
+      viewedItemsCount,
+
+      isItemViewed,
+      addStory,
+      addItemViewed,
+      addSectionViewed,
+      addPlaceViewed,
+      selectLink,
+      lastStoryIndex,
+      setPreviousActive,
+      remove,
+      isVisited,
+      hasVisitedAllLinks,
+      clear,
+    }
   },
-})
+  {
+    persist: true,
+  }
+)
