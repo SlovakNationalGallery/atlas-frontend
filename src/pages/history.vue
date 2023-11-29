907ab38b3852de1650c7cfeb32649a953f2fde84
Mayo Horkovic <mayo.horkovic@gmail.com>
2023-11-29 18:49:00 +0100

    feat: history

---
+++ src/pages/history.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Bucketlist :id="bucketlistId" class="border-b-2" />
+    <History />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Bucketlist from '@/components/history/Bucketlist.vue'
+import History from '@/components/history/History.vue'
+
+const bucketlistId = import.meta.env.VITE_DEFAULT_BUCKETLIST
+</script>
